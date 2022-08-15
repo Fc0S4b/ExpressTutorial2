543dd9738f382fc46bc0a6addf55e408040fd9b8
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-14 21:00:13 -0400

    http request/response cycle

--- final/01-http-basics.js
+++ final/01-http-basics.js
@@ -1,26 +1,31 @@
-const http = require('http')
+//http messages, ciclo de request/response:
+// http request: data que va desde el navegador al servior node/express
+// http response: data que va desde node/express al navegador
+// existirá una respuesta mientras el servidor esté activo
+// cloud server: conjunto de serviores
+const http = require('http');
 
 const server = http.createServer((req, res) => {
   // console.log(req.method)
-  const url = req.url
+  const url = req.url;
   // home page
   if (url === '/') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>home page</h1>')
-    res.end()
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>home page</h1>');
+    res.end();
   }
   // about page
   else if (url === '/about') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>about page</h1>')
-    res.end()
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>about page</h1>');
+    res.end();
   }
   // 404
   else {
-    res.writeHead(404, { 'content-type': 'text/html' })
-    res.write('<h1>page not found</h1>')
-    res.end()
+    res.writeHead(404, { 'content-type': 'text/html' });
+    res.write('<h1>page not found</h1>');
+    res.end();
   }
-})
+});
 
-server.listen(5000)
+server.listen(5000);
