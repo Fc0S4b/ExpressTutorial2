02e6248c6aa3e6848c75dfbe8d022baf0d9e6974
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-16 16:56:52 -0400

    json basics

--- final/06-basic-json.js
+++ final/06-basic-json.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const app = express()
-const { products } = require('./data')
+const express = require('express');
+const app = express();
+const { products } = require('./data');
 app.get('/', (req, res) => {
-  res.json(products)
-})
+  res.json(products); //ahora estás sirviendo products del archivo data como api en el port 5000
+});
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
-})
+  console.log('Server is listening on port 5000....');
+});
