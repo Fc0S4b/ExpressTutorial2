4fdb6d45bfabf24b51d2545e37466b2bec40eeb1
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-16 17:52:53 -0400

    query string

--- final/07-params-query.js
+++ final/07-params-query.js
@@ -36,25 +36,28 @@ app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
   console.log(req.params);
   res.send('hello world');
 });
-
+// query string, usamos un ejemplo de api de algolia versión 1
 app.get('/api/v1/query', (req, res) => {
-  // console.log(req.query)
-  const { search, limit } = req.query;
-  let sortedProducts = [...products];
+  // console.log(req.query)// tendremos todos los parámetros que se ven en la url después de query? en un objeto
+  const { search, limit } = req.query; //parámetros que tiene la api para parámetros de búsqueda y de límite de resultados
+  let sortedProducts = [...products]; //todos los datos de la data products
 
   if (search) {
+    //buscamos si coincide la búsqueda con algún product
     sortedProducts = sortedProducts.filter((product) => {
       return product.name.startsWith(search);
     });
   }
   if (limit) {
+    //manejamos la cantidad de resultados desde 0 hasta el límite indicado
     sortedProducts = sortedProducts.slice(0, Number(limit));
   }
   if (sortedProducts.length < 1) {
     // res.status(200).send('no products matched your search');
-    return res.status(200).json({ sucess: true, data: [] });
+    return res.status(200).json({ success: true, data: [] }); //más común configurar este tipo de respuesta, con success true y un array vacío, ya que la solicitud fue exitosa pero se devuelve nada
+    // si omitimos el return, javascript seguirá ejecutando la siguiente línea, es decir se verá mas de una respuesta para la misma solicitud (error en terminal)
   }
-  res.status(200).json(sortedProducts);
+  res.status(200).json(sortedProducts); //si no hay query? se devuelve todo los productos
 });
 
 app.listen(5000, () => {
