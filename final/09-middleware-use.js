bcc84b684e607a9abd2f99b8ff7aff1676f5c80c
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-16 18:38:45 -0400

    multiple middleware functions

--- final/09-middleware-use.js
+++ final/09-middleware-use.js
@@ -8,7 +8,7 @@ const authorize = require('./authorize');
 //  req => middleware => res
 // con app.use usamos los middlewares para cada ruta, el orden importa donde se invoca app.use, si la usas después de un home, entonces home no tendrá el middleware
 // se puede agregar rutas para aplicar middleware app.use('/api', logger) como base de ruta
-app.use([logger, authorize]);
+app.use([logger, authorize]); //múltiples middlewares, el orden importa, authorize se ejecutará primero si está antes que logger
 // Middleware se aplicará a todo lo que esté después de api
 // api/home/about/products
 app.get('/', (req, res) => {
@@ -21,7 +21,7 @@ app.get('/api/products', (req, res) => {
   res.send('Products');
 });
 app.get('/api/items', (req, res) => {
-  console.log(req.user);
+  console.log(req.user); //podemos ver a user del middleware authorized
   res.send('Items');
 });
 
--- final/10-middleware-options.js
+++ final/10-middleware-options.js
@@ -1,28 +1,35 @@
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
-const logger = require('./logger')
-const authorize = require('./authorize')
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const logger = require('./logger');
+const authorize = require('./authorize');
 //  req => middleware => res
 
 // app.use([logger, authorize])
 // app.use(express.static('./public'))
-app.use(morgan('tiny'))
+// morgan es un middleware de terceros
+app.use(morgan('tiny')); // tiny arroja minimal output
 
 app.get('/', (req, res) => {
-  res.send('Home')
-})
+  res.send('Home');
+});
 app.get('/about', (req, res) => {
-  res.send('About')
-})
+  res.send('About');
+});
 app.get('/api/products', (req, res) => {
-  res.send('Products')
-})
+  res.send('Products');
+});
+// si queremos agregar middleware a solo una req, lo hacemos en el segundo argumento del get
+// app.get('api/items', [logger, authorize], (req, res)=>{})
 app.get('/api/items', (req, res) => {
-  console.log(req.user)
-  res.send('Items')
-})
+  console.log(req.user);
+  res.send('Items');
+});
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
-})
+  console.log('Server is listening on port 5000....');
+});
+
+// opciones de middleware, usar las que creamos, usar las que tiene express o un tercero, para esto dos últimos puedes buscar referencias en docs
+// un ejemplo de middleware de express es static en app.use(express.static('./public'))
+// si es de un tercero, hay que instalarlo, en este ejemplo usamos morgan
