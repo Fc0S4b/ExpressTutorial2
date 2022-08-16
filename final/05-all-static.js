ce35e29717f74cacc96f0cdfcb00b3bb7815dc77
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-16 16:40:33 -0400

    express all static

--- final/05-all-static.js
+++ final/05-all-static.js
@@ -1,21 +1,21 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 
-const app = express()
+const app = express();
 
 // setup static and middleware
-app.use(express.static('./public'))
-
+app.use(express.static('./public'));
+// si index.html está en public entonces se seguirá renderizando todos los archivos estáticos sin necesidad de invocar sendFile
 // app.get('/', (req, res) => {
 //   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
 //   adding to static assets
-//   SSR
+//   SSR (serving side rendering)
 // })
 
 app.all('*', (req, res) => {
-  res.status(404).send('resource not found')
-})
+  res.status(404).send('resource not found');
+});
 
 app.listen(5000, () => {
-  console.log('server is listening on port 5000....')
-})
+  console.log('server is listening on port 5000....');
+});
