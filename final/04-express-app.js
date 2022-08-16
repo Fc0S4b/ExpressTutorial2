b627aa0cf97d19055dbf7d63d339c1b648001153
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-16 16:34:27 -0400

    express app example

--- final/04-express-app.js
+++ final/04-express-app.js
@@ -1,19 +1,20 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+// path viene con node, esto es para ir a la ruta de index.html de la navbar-app
+const path = require('path');
 
-const app = express()
+const app = express();
 
 // setup static and middleware
-app.use(express.static('./public'))
+app.use(express.static('./public')); //de esta forma usa style.css, logo.svg sin tener que hacer la request con los content types. Recuerda usar app.use para setear el middleware. static significa que son archivos que el servidor no tiene que cambiarlos, lo común es dejar estos archivos en la carpeta en public (son estáticos en el server pero pueden ser dinámicos en el navegador)
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-})
+  res.sendFile(path.resolve(__dirname, './navbar-app/index.html')); //puede ser resolve o join
+});
 
 app.all('*', (req, res) => {
-  res.status(404).send('resource not found')
-})
+  res.status(404).send('resource not found');
+});
 
 app.listen(5000, () => {
-  console.log('server is listening on port 5000....')
-})
+  console.log('server is listening on port 5000....');
+});
