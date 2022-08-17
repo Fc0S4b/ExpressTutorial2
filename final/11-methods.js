f7229c81117d58b6061ad756c8a38e8aaef2aaee
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-16 23:22:18 -0400

    post postman, put, delete

--- final/11-methods.js
+++ final/11-methods.js
@@ -22,7 +22,7 @@ app.post('/api/people', (req, res) => {
   }
   res.status(201).json({ success: true, person: name });
 });
-
+// en postman escribimos en el body { "name": "john"} y enviamos la solicitud usando método post, obtendremos todos los datos del array people ya que el name es true
 app.post('/api/postman/people', (req, res) => {
   const { name } = req.body;
   if (!name) {
@@ -41,12 +41,12 @@ app.post('/login', (req, res) => {
 
   res.status(401).send('Please Provide Credentials');
 });
-
+// testeando el método put (para actualizar)
 app.put('/api/people/:id', (req, res) => {
-  const { id } = req.params;
-  const { name } = req.body;
+  const { id } = req.params; //obtenemos el id desde el parámetro de la url
+  const { name } = req.body; //se necesita enviar un name en el body request del postman para actualizar ese item, el body recoge el value a actualizar
 
-  const person = people.find((person) => person.id === Number(id));
+  const person = people.find((person) => person.id === Number(id)); //buscamos si existe ese name que se pide en el req dentro del array people, se convierte en número el id ya que está en string
 
   if (!person) {
     return res
@@ -55,15 +55,15 @@ app.put('/api/people/:id', (req, res) => {
   }
   const newPeople = people.map((person) => {
     if (person.id === Number(id)) {
-      person.name = name;
+      person.name = name; //si person existe entonces le cambiamos el nombre al valor entregado de name en el body
     }
     return person;
   });
   res.status(200).json({ success: true, data: newPeople });
 });
-
+// método delete, similar a put a excepción de que no esperamos nada del body, solo eliminamos lo que piden en el req
 app.delete('/api/people/:id', (req, res) => {
-  const person = people.find((person) => person.id === Number(req.params.id));
+  const person = people.find((person) => person.id === Number(req.params.id)); //accediendo a los params directamente en vez de destructurar
   if (!person) {
     return res
       .status(404)
@@ -71,7 +71,7 @@ app.delete('/api/people/:id', (req, res) => {
   }
   const newPeople = people.filter(
     (person) => person.id !== Number(req.params.id)
-  );
+  ); //buscamos si existe el person que se pide en el req
   return res.status(200).json({ success: true, data: newPeople });
 });
 
